1c7f0dd99e9139e55e4d07a63cb623b51fcdc805
Little language <47820811+Sereoj@users.noreply.github.com>
2021-02-02 18:47:18 +0300

    unary_expression

---
+++ ConsoleC8NetCore/Array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleC8NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Array
+             * unary_expression
+             */
+            var words = new string[]
+            {
+                "Каждый",   //0 || ^7
+                "охотник",  //1 || ^6
+                "желает",   //2 || ^5
+                "знать",    //3 || ^4
+                "где",      //4 || ^3
+                "сидит",    //5 || ^2
+                "фазан",    //6 || ^1
+            };
+
+            Console.WriteLine($"{words[^7]} {words[^1]}");
+          
+        }
+    }
+}
