96842aa41e7c8f371195ac013711a19e13d5b617
Little language <47820811+Sereoj@users.noreply.github.com>
2021-02-02 15:59:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleC8NetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleC8NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
